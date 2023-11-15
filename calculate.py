97cdce02ec712b103802c8b179cc0823d5c73113
echang1 <shurueichang@gmail.com>
2023-11-15 11:54:56 -0500

    updated example notebooks

--- calculate.py
+++ calculate.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import torch
 import torch.nn as nn
 from scipy.special import digamma
 from sklearn.metrics import mutual_info_score
@@ -50,6 +52,67 @@ def mine(x, y, fsize=100, batch_size=100):
     statistics_network = nn.Sequential(nn.Linear(dimX+dimY, fsize), nn.ReLU(), nn.Linear(fsize, fsize), nn.ReLU(), nn.Linear(fsize, fsize), nn.ReLU(), nn.Linear(fsize, 1))
     mine = utils.Mine(T=statistics_network, loss = 'fdiv', method = 'concat')
     return mine.optimize(x, y, batch_size=batch_size, iters=100)
+
+#from mine.models.mine.py
+class Mine(nn.Module):
+    def __init__(self, T, loss='mine', alpha=0.01, method=None):
+        super().__init__()
+        self.running_mean = 0
+        self.loss = loss
+        self.alpha = alpha
+        self.method = method
+
+        if method == 'concat':
+            if isinstance(T, nn.Sequential):
+                self.T = utils.CustomSequential(utils.ConcatLayer(), *T)
+            else:
+                self.T = utils.CustomSequential(utils.ConcatLayer(), T)
+        else:
+            self.T = T
+
+    def forward(self, x, z, z_marg=None):
+        if z_marg is None:
+            z_marg = z[torch.randperm(x.shape[0])]
+
+        t = self.T(x, z).mean()
+        t_marg = self.T(x, z_marg)
+
+        if self.loss in ['mine']:
+            second_term, self.running_mean = utils.ema_loss(t_marg, self.running_mean, self.alpha)
+        elif self.loss in ['fdiv']:
+            second_term = torch.exp(t_marg - 1).mean()
+        elif self.loss in['mine_biased']:
+            second_term = torch.logsumexp(t_marg, 0) - math.log(t_marg.shape[0])
+
+        return -t + second_term
+
+    def mi(self, x, z, z_marg=None):
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x).float()
+        if isinstance(z, np.ndarray):
+            z = torch.from_numpy(z).float()
+
+        with torch.no_grad():
+            mi = -self.forward(x, z, z_marg)
+        return mi
+
+    def optimize(self, X, Y, iters, batch_size, opt=None):
+
+        if opt is None:
+            opt = torch.optim.Adam(self.parameters(), lr=1e-4)
+           
+        for iter in range(1, iters + 1):
+            mu_mi = 0
+            for x, y in utils.batch(X, Y, batch_size):
+                opt.zero_grad()
+                loss = self.forward(x, y)
+                loss.backward()
+                opt.step()
+                mu_mi -= loss.item()
+            if iter % (iters // 3) == 0:
+                pass
+        final_mi = self.mi(X, Y)
+        return final_mi.item()
 
 
 
